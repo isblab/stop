7c220504ce1b99c3afd76d377f80505b83e5e48b
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2021-10-08 15:02:30 +0530

    fixed tests

--- tests/run_test.py
+++ tests/run_test.py
@@ -5,6 +5,20 @@ import os
 import shutil
 
 
+def test_analyzer():
+    os.chdir('./tests')
+    os.mkdir('./temp_data')
+    sys.path = ['..'] + sys.path
+    import analyzer
+    data = [f'tests/test_data/output{x}' for x in range(1, 4)]
+    ans = analyzer.DefaultAnalysis(data, ['ma', 'mb'],
+                                   ['GaussianEMRestraint_None', 'CrossLinkingMassSpectrometryRestraint_Distance_'],
+                                   './temp_data')
+    assert ans[0]
+    assert all([(x in ans[2]) for x in ['ma', 'mb']])
+    assert all([(ans[2][x][1] < 1e-8) for x in ['ma', 'mb']])
+
+
 def test_runexamples():
     os.chdir('./tests')
     os.mkdir('./temp_data')
@@ -23,21 +37,3 @@ def test_runexamples():
     with open('./temp_data/logs/report.txt') as f:
         rd = f.read().split('\n')
         assert len([x for x in rd if 'Optimization Status: Successful' in x]) == 2, rd
-    shutil.rmtree('./temp_data')
-    os.mkdir('./temp_data')
-    main.main(['', 'example_param_file3'])
-    assert os.path.isfile('./temp_data/logs/report.txt')
-
-
-def test_analyzer():
-    os.chdir('./tests')
-    os.mkdir('./temp_data')
-    sys.path = ['..'] + sys.path
-    import analyzer
-    data = [f'tests/test_data/output{x}' for x in range(1, 4)]
-    ans = analyzer.DefaultAnalysis(data, ['ma', 'mb'],
-                                   ['GaussianEMRestraint_None', 'CrossLinkingMassSpectrometryRestraint_Distance_'],
-                                   './temp_data')
-    assert ans[0]
-    assert all([(x in ans[2]) for x in ['ma', 'mb']])
-    assert all([(ans[2][x][1] < 1e-8) for x in ['ma', 'mb']])
