625058ad48e7b0776ea47ffb924bc0ad2770dfce
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2021-10-08 13:47:02 +0530

    Fixed tests & updated workflow

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,16 +1,28 @@
 import pytest
-import sys
 import subprocess
 import os
+import shutil
+
 
 def test_runexamples():
-    s = subprocess.run(['python', 'main.py', 'example_param_file'], text=True, capture_output=True)
-    assert s.returncode == 1, s.stdout + s.stderr
-    assert s.stderr == '', s.stderr
-    s = subprocess.run(['python', 'main.py', 'example_param_file2'], text=True, capture_output=True)
-    assert s.returncode == 1, s.stdout + s.stderr
-    assert s.stderr == '', s.stderr
-    s = subprocess.run(['python', 'main.py', 'example_param_file3'], text=True, capture_output=True)
-    assert s.returncode == 1, s.stdout + s.stderr
-    assert s.stderr == '', s.stderr
-    
+    os.chdir('./tests')
+    os.mkdir('./temp_data')
+    s = subprocess.run(['python', '../main.py', 'example_param_file'], text=True, capture_output=True)
+    assert s.returncode == 0, s.stderr
+    assert os.path.isfile('./temp_data/logs/report.txt'), s.stderr
+    with open('./temp_data/logs/report.txt') as f:
+        rd = f.read().split('\n')
+        assert len([x for x in rd if 'Optimization Status: Successful' in x]) == 4, rd
+        assert len([x for x in rd if 'Optimization Status: Failed' in x]) == 2, rd
+    shutil.rmtree('./temp_data')
+    os.mkdir('./temp_data')
+    s = subprocess.run(['python', '../main.py', 'example_param_file2'], text=True, capture_output=True)
+    assert s.returncode == 0, s.stderr
+    with open('./temp_data/logs/report.txt') as f:
+        rd = f.read().split('\n')
+        assert len([x for x in rd if 'Optimization Status: Successful' in x]) == 4, rd
+        assert len([x for x in rd if 'Optimization Status: Failed' in x]) == 2, rd
+    shutil.rmtree('./temp_data')
+    os.mkdir('./temp_data')
+    s = subprocess.run(['python', '../main.py', 'example_param_file3'], text=True, capture_output=True)
+    assert s.returncode == 0, s.stderr
