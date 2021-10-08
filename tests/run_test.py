a027fb3fd23e29ce348d4ef43fa68f0709b608d5
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2021-10-08 15:05:38 +0530

    changed test paths

--- tests/run_test.py
+++ tests/run_test.py
@@ -6,11 +6,9 @@ import shutil
 
 
 def test_analyzer():
-    os.chdir('./tests')
     os.mkdir('./temp_data')
-    sys.path = ['..'] + sys.path
     import analyzer
-    data = [f'tests/test_data/output{x}' for x in range(1, 4)]
+    data = [f'./tests/test_data/output{x}' for x in range(1, 4)]
     ans = analyzer.DefaultAnalysis(data, ['ma', 'mb'],
                                    ['GaussianEMRestraint_None', 'CrossLinkingMassSpectrometryRestraint_Distance_'],
                                    './temp_data')
