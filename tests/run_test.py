195485e92eab5b11c12ce594431a2c49ad6b6d0c
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2022-01-31 22:38:54 +0530

    broke tests into chunks

--- tests/run_test.py
+++ tests/run_test.py
@@ -21,7 +21,7 @@ def test_analyzer():
     os.chdir('..')
 
 
-def test_runexamples():
+def test_runexamples_1():
     os.chdir('./tests')
     os.mkdir('./temp_data')
     s = subprocess.run(['python', '../main.py', 'example_param_file'], text=True, capture_output=True)
@@ -32,6 +32,11 @@ def test_runexamples():
         assert len([x for x in rd if 'Optimization Status: Successful' in x]) == 4, rd
         assert len([x for x in rd if 'Optimization Status: Failed' in x]) == 2, rd
     shutil.rmtree('./temp_data')
+    os.chdir('..')
+
+
+def test_runexamples_2():
+    os.chdir('./tests')
     os.mkdir('./temp_data')
     s = subprocess.run(['python', '../main.py', 'example_param_file2'], text=True, capture_output=True)
     assert s.returncode == 0
@@ -40,6 +45,11 @@ def test_runexamples():
         rd = f.read().split('\n')
         assert len([x for x in rd if 'Optimization Status: Successful' in x]) == 2, rd
     shutil.rmtree('./temp_data')
+    os.chdir('..')
+
+
+def test_runexamples_3d():
+    os.chdir('./tests')
     os.mkdir('./temp_data')
     s = subprocess.run(['python', '../main.py', 'example_param_file3d'], text=True, capture_output=True)
     assert s.returncode == 0
@@ -48,6 +58,11 @@ def test_runexamples():
         rd = f.read().split('\n')
         assert len([x for x in rd if 'Optimization Status: Successful' in x]) == 1, rd
     shutil.rmtree('./temp_data')
+    os.chdir('..')
+
+
+def test_runexamples_3():
+    os.chdir('./tests')
     os.mkdir('./temp_data')
     s = subprocess.run(['python', '../main.py', 'example_param_file3'], text=True, capture_output=True)
     assert s.returncode == 0
