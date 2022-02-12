6fed886b64f02fe3e5dd3a4d20a1ba6c4b64960d
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2022-02-12 16:06:47 +0530

    dynamic color for coverage badge

--- tests/json_parse.py
+++ tests/json_parse.py
@@ -1,5 +1,21 @@
 import json
+import sys
 
 with open('coverage.json') as f:
     j = json.loads(f.read())
-print(round(float(j['totals']['percent_covered']), 2))
+
+val = round(float(j['totals']['percent_covered']), 2)
+
+if val >= 85:
+    col = 'green'
+elif val >=50:
+    col = 'yellow'
+elif val >= 30:
+    col = 'orange'
+else:
+    col = 'red'
+
+if '--color' in sys.argv:
+    print(col)
+else:
+    print(val)
